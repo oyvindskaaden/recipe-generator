7520b1f2866f44b3be1facbab1fd0684510c2e00
Øyvind Skaaden <oyvindskaaden@gmail.com>
2022-01-02 21:01:56 +0100

    Somewhat working generator using jinja2

---
+++ recipe-generator.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+import json
+
+from jinja2 import Environment, select_autoescape
+from jinja2.environment import Template
+from jinja2.loaders import FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader("templates"),
+    autoescape=select_autoescape()
+)
+
+parser = argparse.ArgumentParser(description="Generate recipes within a directory tree to different formats")
+parser.add_argument('source_dir', metavar='dir', help="The directory in which the source files are located.")
+
+args = parser.parse_args()
+
+print(args.source_dir)
+
+filelist = []
+dirlist = []
+for root, dirs, files in os.walk(f"./{args.source_dir}"):
+    for file in files:
+        if(file.endswith(".json")):
+            filelist.append(os.path.join(root,file))
+            if root not in dirlist:
+                dirlist.append(root)
+
+print(filelist)
+print(dirlist)
+
+for dir in dirlist:
+    os.makedirs(os.path.join("./_generated", dir), mode=0o775, exist_ok=True)
+
+def generateTemplate(template: Template):
+    file = open("examples/pizzadeig.json")
+
+    data = json.load(file)
+
+    file.close()
+
+    return template.render(
+        title=data['title'], 
+        description=data['description'],  
+        interactive=True,
+        portions=data['portions'],
+        ingredients=data['ingredients'],
+        steps=data['steps']
+    )
+
+#print(generateTemplate(env.get_template("jekyll/jekyll.md.jinja")))
